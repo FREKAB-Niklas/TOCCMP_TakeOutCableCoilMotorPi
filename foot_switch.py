bf814591067443917ed9180a05b64874f596fdd8
FREKAB-Niklas <niklas@frekab.se>
2024-08-05 16:28:29 +0200

    test

--- foot_switch.py
+++ foot_switch.py
@@ -3,7 +3,7 @@ import time
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-FOOT_SWITCH_PIN = 27
+FOOT_SWITCH_PIN = 22  # Change to GPIO 22
 
 # Set up the pin as an input with a pull-down resistor
 GPIO.setup(FOOT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -15,7 +15,10 @@ def foot_switch_callback(channel):
 GPIO.add_event_detect(FOOT_SWITCH_PIN, GPIO.RISING, callback=foot_switch_callback, bouncetime=200)
 
 try:
+    print("Waiting for foot switch press...")
     while True:
+        if GPIO.input(FOOT_SWITCH_PIN) == GPIO.HIGH:
+            print("Foot switch pressed! (while loop detection)")
         time.sleep(0.1)  # Main loop doing nothing, just waiting for the interrupt
 
 except KeyboardInterrupt:
