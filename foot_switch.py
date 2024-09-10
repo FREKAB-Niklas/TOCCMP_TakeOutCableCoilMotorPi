0e5656d8878ae0379a0d6e48119973244983e792
FREKAB-Niklas <niklas@frekab.se>
2024-09-10 15:11:50 +0200

    test

--- foot_switch.py
+++ foot_switch.py
@@ -87,7 +87,7 @@ try:
             print("Long press detected. Running Motor 2 until stop button is pressed.")
             
             GPIO.output(ENABLE_PIN_M1, GPIO.HIGH)  # Enable the motor
-            GPIO.output(ENABLE_PIN_M2, GPIO.LOW)  # Disable the motor (LOW is enable for most drivers)
+            GPIO.output(ENABLE_PIN_M2, GPIO.HIGH)  # Disable the motor (LOW is enable for most drivers)
             try:
                 while GPIO.input(STOP_BUTTON) == GPIO.HIGH:
                     move_motor_steps(1, GPIO.HIGH, STEP_PIN_M2, DIR_PIN_M2, DELAY_M2)
@@ -99,6 +99,7 @@ try:
                 reset_motor_driver()
             finally:
                 GPIO.output(ENABLE_PIN_M1, GPIO.LOW)  # Disable the motor
+                GPIO.output(ENABLE_PIN_M2, GPIO.LOW)  # Disable the motor
             
         else:
             print("Short press detected. Moving Motor 1 forward 1000 steps.")
